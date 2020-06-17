95fad4850dd5175a66dbeb227578b851cf691812
svadivazhagu <svadivazhagu@wpi.edu>
2020-06-17 15:37:38 -0400

    retriever works, working on setting up database now

--- README.md
+++ README.md
@@ -5,5 +5,13 @@ Data science projects to visualize coronavirus data.
 ## TODO
     Create database in SQLalchemy
     create ingestion pipeline to automatically fetch data from the NYT github repo
+        https://raw.githubusercontent.com/nytimes/covid-19-data/master/
+            us-states.csv
+            us-counties.csv
+            us.csv
+
     Create API in flask
     Figure out what graphs I want to use
+
+
+
--- app.py
+++ app.py
@@ -1,2 +1,18 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.states'
+
+
+db = SQLAlchemy(app)
+
+
+class Record(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, default=datetime.now)
+
+
