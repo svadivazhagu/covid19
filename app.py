61ddf4d8895d334df8d58edf91e7ffd587a94e4f
Surya Vadivazhagu <svadivazhagu@wpi.edu>
2020-06-19 14:33:06 -0400

    added types for the columns

--- app.py
+++ app.py
@@ -13,6 +13,11 @@ db = SQLAlchemy(app)
 
 class Record(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    insert_date = db.Column(db.DateTime, default=datetime.now)
     date = db.Column(db.DateTime, default=datetime.now)
+    state = db.Column(db.String(50))
+    fips = db.Column(db.Integer)
+    cases = db.Column(db.Integer)
+    deaths = db.Column(db.Integer)
 
 
