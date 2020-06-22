6f6063dc9e65d339649008bb81285843f69c9ac8
Surya Vadivazhagu <svadivazhagu@wpi.edu>
2020-06-22 01:17:39 -0400

    database is done being set up, now working on setting up API.

---
+++ DbHandler.py
@@ -0,0 +1,67 @@
+# File that handles the pulling of Data from GitHub, and insertion into the Database cases.db
+import sqlite3
+from sqlite3 import Error
+import pandas as pd
+import requests as re
+
+
+def create_conn(db):
+    '''
+    Creates connection with the SQlite database
+    :param db:
+    :return: Connection object or None
+    '''
+    conn = None
+    try:
+        conn = sqlite3.connect(db)
+    except Error as e:
+        print(e)
+    return conn
+
+
+def create_table(conn, query):
+    '''
+    Creates a table given the connection and the table parameters
+    :param conn:
+    :param query:
+    :return: Nothing
+    '''
+    try:
+        c = conn.cursor()
+        c.execute(query)
+    except Error as e:
+        print(e)
+
+
+def csv_sql(name, csv_fp, conn):
+    df = pd.read_csv(csv_fp)
+    df.columns = df.columns.str.strip()
+    df.to_sql(name, conn, if_exists='append')
+    #print(name + ' inserted.')
+
+
+def main():
+    db = 'cases.db'
+    conn = create_conn(db)
+
+    # create tables
+    if conn is not None:
+        fp = 'data/'
+        for i in ['country', 'states', 'counties']:
+            csv_sql(i, fp + i + '.csv', conn)
+
+    else:
+        print("Error, connection failed")
+
+
+if __name__ == '__main__':
+    mapping = {'country': re.get('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us.csv'),
+               'states': re.get('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv'),
+               'counties': re.get('https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv')}
+
+    for i in mapping.keys():
+        with open('data/' + i + '.csv', 'wb') as f:
+            f.write(mapping[i].content)
+        #print(i + ' downloaded.')
+
+    main()
--- app.py
+++ app.py
@@ -1,23 +1,30 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
 
 app = Flask(__name__)
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.states'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cases.db'
 
 
 db = SQLAlchemy(app)
 
+# class CountryCases(db.Model):
+#     id = db.Column(db.Integer, primary_key=True)
+#     date = db.Column(db.String(10))
+#     cases = db.Column(db.Integer)
+#     deaths = db.Column(db.Integer)
 
-class Record(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    state = db.Column(db.String(30))
-    date = db.Column(db.Date)
-    fips = db.Column(db.Integer)
-    cases = db.Column(db.Integer)
-    deaths = db.Column(db.Integer)
 
+# #Route for inserting new data COUNTRY-wide into the DB
+# @app.route('/<date>/<cases>/<deaths>')
+# def index(date, cases, deaths):
+#     countryCases = CountryCases(date = date, cases = cases, deaths = deaths)
+#     db.session.add(countryCases)
+#     db.session.commit()
+#
+#     return '<h1>Added new row</h1>'
+#
+#
 
 
